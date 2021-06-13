9e62601f389858b1ddb77b8a920fbe3efe493a1d
source-ali <alichoker.marketing@gmail.com>
2021-06-13 02:34:25 -0700

    added error handling to config and helper files

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,6 +1,16 @@
+import { TIMEOUT_SECONDS } from './config';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
 export const getJSON = async function (url) {
   try {
-    const response = await fetch(url);
+    const response = await Promise.race([fetch(url), timeout(TIMEOUT_SECONDS)]);
 
     const data = await response.json();
 
